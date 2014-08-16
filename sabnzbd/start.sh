a3149639373048b0ed927e48286bb73b69354d80
lvlie <leon@omfg.nl>
2014-08-16 20:06:57 +0200

    Replaced sabnzbd config-dir to /config/sabnzbd

--- sabnzbd/start.sh
+++ sabnzbd/start.sh
@@ -5,7 +5,8 @@ test -d /config || exit 1
 # same goes for data
 test -d /data || exit 2
 
-sabnzbdplus --daemon --config-file /config -s 0.0.0.0:8080
+sabnzbdplus --daemon --config-file /config/sabnzbd -s 0.0.0.0:8080
+
 sleep 5
 
 tail -f /config/logs/sabnzbd.*
