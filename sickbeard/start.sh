a840f96847ddfd2b443633ed042d6e8950c06c5b
lvlie <leon@omfg.nl>
2014-08-16 20:21:14 +0200

    Adjusted sickbeard to use /config and /data and added start.sh

---
+++ sickbeard/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# if /config doesnt exist, exit
+test -d /config || exit 1
+# same goes for data
+test -d /data || exit 2
+# create /config/sickbeard if it doesnt exist
+test -d /config/sickbeard || mkdir /config/sickbeard
+
+cd /sickbeard
+
+for file in config.ini sickbeard.db
+ do
+	if [ -f /config/sickbeard/${file} ]
+	# if $file is in /config/sickbeard, remove existing one in docker image
+	 then
+		rm -rf /sickbeard/${file}
+	# otherwise move the one from docker image to /config/sickbeard
+	 else
+		mv -f /sickbeard/${file} /config/sickbeard/${file}
+	fi
+done
+
+# link /config version to /sickbeard
+ln -sf /config/sickbeard/config.ini /sickbeard/config.ini
+ln -sf /config/sickbeard/sickbeard.db /sickbeard/sickbeard.db
+
+# start sickbeard
+/usr/bin/python SickBeard.py
