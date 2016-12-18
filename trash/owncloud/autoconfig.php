7f27071420a28a1fd632ac91ef9ab88b9594676f
lvlie <leon@omfg.nl>
2016-12-18 14:42:37 +0100

    Rehaul: removed base container, added restart=always, moved to plex upstream yum repo instead of rpm link, removed old docker containers

--- trash/owncloud/autoconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$AUTOCONFIG = array(
-  "dbtype"        => "sqlite",
-  "dbname"        => "owncloud",
-  "dbtableprefix" => "",
-  "adminlogin"    => "admin",
-  "adminpass"     => "changeme",
-  "directory"     => "/data/owncloud",
-);
