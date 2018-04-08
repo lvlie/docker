8587bb004b27e7dd526226d92357f42f5b6c0b7e
Leon Vliegenthart <leon@omfg.nl>
2018-04-08 21:25:29 +0200

    Fix httpd selinux thingy

--- proxy/start.sh
+++ proxy/start.sh
@@ -5,6 +5,8 @@ test -d /config/httpd || exit 1
 
 # copy conf
 cp /config/httpd/proxy.conf /etc/httpd/conf.d/
+mkdir /etc/httpd/secret && chown 640 /etc/httpd/secret
+cp /config/httpd/.htpw /etc/httpd/secret/.htpw
 
 # sleep 4
 sleep 4
